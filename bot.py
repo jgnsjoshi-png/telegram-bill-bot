97e57329b3f325ec0b872fec5fc3b0ea2d1da348
jgnsjoshi-png <jgnsjoshi@gmail.com>
2026-02-01 19:14:44 +0530

    Change PDF_URL to new OneDrive link
    Updated PDF_URL to a new OneDrive link.

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from PyPDF2 import PdfReader, PdfWriter
 TOKEN = os.getenv("TOKEN")
 CSV_FILE = "consumers.csv"
 PDF_FILE = "bills.pdf"
-PDF_URL = "https://drive.google.com/file/d/1A2RNG8qc_JboJ7Y55uzDvIbbq1cF2zfS/view?usp=drivesdk"
+PDF_URL = "https://1drv.ms/b/c/1C9BAE5D707AC247/IQDpWA7nyLI6RI_rmF2Z8V5KAWybDX6a7rgBFzyTG0XgWSw"
 
 def download_pdf():
     if not os.path.exists(PDF_FILE):
